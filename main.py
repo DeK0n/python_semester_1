4ef61fb5dd4941b7d4da9cc3701f32bbb519b565
DeK0n <dv.knnv@gmail.com>
2022-08-22 15:23:49 +0300

    Commit and push 2

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello everybody!")
-print("Hello, everybody 2!")
-
+weight_str = input('Enter weight in kilograms:')
+height_str = input('Enter height in centimeters:')
+weight = float(weight_str)
+height = float(height_str)
+mass_index = (weight / (height*height) * 10000)
+print(f"BMI (body mass index) is : {mass_index: 6.1f}")
