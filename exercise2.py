1c61efd807a3479869dd2c13a75569dbcb4d4bf9
DeK0n <dv.knnv@gmail.com>
2022-08-25 19:28:34 +0300

    Commit&push

--- exercise2.py
+++ exercise2.py
@@ -27,7 +27,7 @@ weight_pounds = float(input('Enter pounds: '))
 weight_lots = float(input('Enter lots: '))
 weight_total_in_gramms = ((weight_talents*20+weight_pounds)*32+weight_lots)*13.3
 weight_kilos_int = weight_total_in_gramms//1000 # "//" is division for integer result
-weight_gramms_fractional = weight_total_in_gramms - weight_kilos_int*1000
+weight_gramms_fractional = weight_total_in_gramms - weight_kilos_int*1000 # "-" is minus sign
 print("The weight in modern units is: ")
 print(f"{weight_kilos_int:.0f} kg and {weight_gramms_fractional: .2f} g")
 
