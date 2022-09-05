4c793edf0314f20768495a9d054b2fbad0643770
DeK0n <dv.knnv@gmail.com>
2022-09-05 11:32:17 +0300

    1

--- exercise4.py
+++ exercise4.py
@@ -1,5 +1,8 @@
 # 1 Numbers divided by 3
 
+from turtle import position
+import math
+import random
 number = 1
 while number <= 1000:
     modulo = number % 3
@@ -9,7 +12,7 @@ while number <= 1000:
 
 # 2 Converter
 inches_input = float(input("Enter length in inches: "))
-while inches_input >=0:
+while inches_input >= 0:
     centimetres = inches_input * 2.54
     print(f"This lenght in cm is: {centimetres:.2f}")
     inches_input = float(input("Enter length in inches: "))
@@ -19,21 +22,21 @@ while inches_input >=0:
 input_number = float(input("Enter 1st number: "))
 smallest_number = input_number
 largest_number = input_number
-while input_number !="":
-    if input_number <=smallest_number:
+while input_number != "":
+    if input_number <= smallest_number:
         smallest_number = input_number
-    if input_number >=largest_number:
+    if input_number >= largest_number:
         largest_number = input_number
     input_number = input("Enter next number or leave empty string to finish: ")
     if input_number == "":
         break
     input_number = float(input_number)
-print(f"The smallest number is: {smallest_number: 0.10f} and the largest number is: {largest_number: 0.10f}")
+print(
+    f"The smallest number is: {smallest_number: 0.10f} and the largest number is: {largest_number: 0.10f}")
 
 # 4 Guessing game
 
-import random
-random_integer = random.randint(1,10)
+random_integer = random.randint(1, 10)
 player_integer = int(input("Guess generated integer from 1 to 10: "))
 while player_integer != random_integer:
     if player_integer < random_integer:
@@ -50,11 +53,11 @@ pw = "rules"
 tries = 0
 input_username = ""
 input_password = ""
-while not(input_username == un and input_password == pw):
+while not (input_username == un and input_password == pw):
     tries = tries+1
     input_username = input("Enter user name: ")
     input_password = input("Enter password: ")
-    if tries ==5:
+    if tries == 5:
         print("Acsess denied")
         break
     elif input_username == un and input_password == pw:
@@ -63,9 +66,6 @@ while not(input_username == un and input_password == pw):
         print("Try again")
 
 # 6 Pi
-import math
-import random
-from turtle import position
 operation_num = 0
 n = 0
 N = int(input("Enter number of points to generate: "))
@@ -75,6 +75,6 @@ while operation_num != N:
     point_y = random.uniform(-1, 1)
     position = point_x*point_x+point_y*point_y
     if position < 1:
-        n=n+1
+        n = n+1
 pi = 4*n/N
 print(pi)
---
+++ exercise5.py
@@ -0,0 +1,34 @@
+# 1
+# Write a program that asks the user how many dice to roll. The program rolls all the dice once and prints out the
+# the sum of the numbers. Use a for loop.
+
+# import random
+# dice_list = []
+# dice_sum = 0
+# rolls_total = int(input("How many times you want to throw dice?: "))
+# rolls = 0
+# while rolls != rolls_total:
+#     rolls = rolls+1
+#     dice_list.append(int(random.randint(1, 6)))
+# for x in dice_list:
+#     dice_sum = dice_sum+x
+# print("The sum of all throws is: " + str(dice_sum) +" ("+ str(dice_list)+")")
+
+
+# 2
+# Write a program that asks the user to enter numbers until they input an empty string to quit. At the end,
+#  the program prints out the five greatest numbers sorted in descending order. Hint: You can reverse the order
+#  of sorted list items by using the sort method with the reverse=True argument.
+
+# 3
+# Write a program that asks the user for an integer and tells if the number is a prime number. Prime numbers are
+#  number that are only divisible by one or the number itself.
+
+# For example, number 13 is a prime number as can only be divided by 1 or 13 so that the result is an integer.
+# On the other hand, number 21 for example is not a prime number as it can be also be divided by numbers 3 and 7.
+
+# 4
+# Write a program that asks the user to enter the names of five cities one by on (use a for loop for reading the names)
+#  and stores them into a list structure. Finally, the program prints out the names of the cities one by one, one city
+#  per line, in the same order they were read as input. Use a for loop for asking the names and a for/in loop to iterate
+#  through the list.
