bf2219778138826eb12f606fc2f4a6fda23f83f9
DeK0n <dv.knnv@gmail.com>
2022-09-05 12:55:34 +0300

    1

--- exercise5.py
+++ exercise5.py
@@ -20,6 +20,17 @@
 #  the program prints out the five greatest numbers sorted in descending order. Hint: You can reverse the order
 #  of sorted list items by using the sort method with the reverse=True argument.
 
+input_list = []
+print("Input more than 5 numbers to get them sorted. Input empty string to quit.")
+number_input = input("Input first number: ")
+while number_input != "":
+    input_list.append(number_input)
+    number_input = input("Input next number or press Enter to quit: ")
+input_list.sort(reverse=True)
+print("Five greates numbers in descending order are: ")
+for s in input_list[0:5]:
+    print(s)
+
 # 3
 # Write a program that asks the user for an integer and tells if the number is a prime number. Prime numbers are
 #  number that are only divisible by one or the number itself.
