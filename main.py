b30b96fe3445eb21b96409b28bdbbb426817bb31
DeK0n <dv.knnv@gmail.com>
2022-09-09 23:00:49 +0300

    commit testing

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-integer=int(input("Type in integer:"))
-factorial=1
-if integer <=0:
-    print("Your integer is <= 0, so bye bye!")
-while integer>0:
-    for x in range(1,(integer+1)):
-      factorial=factorial*integer
-      integer=integer-1
-    print("The factorial of your integer is: " +str(factorial))
-    factorial=1
-    integer=int(input("Type in integer:"))
-    
+# integer=int(input("Type in integer:"))
+# factorial=1
+# if integer <=0:
+#     print("Your integer is <= 0, so bye bye!")
+# while integer>0:
+#     for x in range(1,(integer+1)):
+#       factorial=factorial*integer
+#       integer=integer-1
+#     print("The factorial of your integer is: " +str(factorial))
+#     factorial=1
+#     integer=int(input("Type in integer:"))
+     
