490a339f101f424b84b3ba31c15e3cf58b25a536
DeK0n <dv.knnv@gmail.com>
2022-09-15 14:11:08 +0300

    c

--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-import math
-n = int(input("number?"))
-for i in range(2, int(math.sqrt(n))+1):
-    if n % i == 0:
-        print(f"dividable by {i}")
-        break
-else:
-    print("prime")
+#testin file
