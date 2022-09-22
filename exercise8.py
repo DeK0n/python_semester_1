65c2eef06c699e84a3f9021f22e1b02865a78fd3
DeK0n <dv.knnv@gmail.com>
2022-09-22 21:00:48 +0300

    commint message

--- exercise8.py
+++ exercise8.py
@@ -3,6 +3,32 @@
 # The program fetches and prints out the corresponding airport name and location (town)
 # from the airport database used on this course.
 
+print("Input ICAO code to get iformation: ")
+
+import mysql.connector
+
+def getInfoBy(icao):
+    sql = "SELECT name from airport where ident ='" + icao + "'"
+    # print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result) 
+    return
+
+# use your user and password, host can be localhost or 127.0.0.0
+connection = mysql.connector.connect(
+         host='localhost',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='12332167',
+         autocommit=True
+         )
+
+icao = input("Enter ICAO: ")
+getInfoBy(icao)
+
 #2
 # Write a program that asks the user to enter the area code (for example FI)
 #  and prints out the airports located in that country ordered by airport type. 
