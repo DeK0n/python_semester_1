33abe212a11a19823a67a22e9bc4365d5147f2f8
DeK0n <dv.knnv@gmail.com>
2022-10-12 23:08:30 +0300

    commit

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 # Testing grounds
+from geopy import distance
+from unittest import result
+import mysql.connector
+
+def listEurope():
+    sql = 'SELECT name from countries where continent = "EU"'
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    print(result)
+    return
+
+
+# use your user and password, host can be localhost or 127.0.0.0
+connection = mysql.connector.connect(
+    host='localhost',
+    port=3306,
+    database='flight_game',
+    user='',
+    password='',
+    autocommit=True
+)
+
+listEurope()
